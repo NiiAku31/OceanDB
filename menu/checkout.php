3dbbd70e3616af7055093c5044596ad791bd6175
NiiAku31 <77838552+NiiAku31@users.noreply.github.com>
2021-12-11 00:55:46 +0000

    Update checkout.php

--- menu/checkout.php
+++ menu/checkout.php
@@ -42,7 +42,7 @@
 <body>
 	<div class="main-menu sticky-top">
 		<div class="logo">
-			<img src="/Oceandb/new/oceanDB.jpeg" alt="site logo" class="img-fluid">
+			<img src="/OceanDB/new/oceanDB.jpeg" alt="site logo" class="img-fluid">
 		</div>
 		<div class="inner-menu sticky-top">
 			<a href="index.php" style="float: left;">MENU</a>
